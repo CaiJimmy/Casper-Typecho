7d1ac99be6989b9bd2da52d190a1032c66fe07d0
JimmyCai <jinqingcai@hotmail.com>
2015-06-07 10:54:02 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
         <footer class="site-footer clearfix">
             <section class="copyright"><a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a> &copy; <?php echo date( 'Y'); ?></section>
-            <section class="poweredby">Proudly published with <a href="http://typecho.org">Typecho</a>. Theme from <a href="https://ghost.org" target="_blank">Ghost</a> & <a href="http://nichijou.org" target="_blank">Jimmy</a></section>
+            <section class="poweredby">Proudly published with <a href="http://typecho.org">Typecho</a>. Theme from <a href="https://ghost.org" target="_blank">Ghost</a> & <a href="http://jimmycai.org" target="_blank">Jimmy</a></section>
         </footer>
 
     </div>
